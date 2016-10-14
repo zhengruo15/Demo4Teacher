4d6b88ac6b469a766bdac2d1fcfd3c2b76b3d009
zhengruo15 <ruoyu.zheng@quantil.com>
2016-10-14 12:38:38 +0800

    Signed-off-by: zhengruo15 <ruoyu.zheng@quantil.com>

--- target/Demo4Teacher-0.0.1-SNAPSHOT/WEB-INF/js/studentDetail.js
+++
@@ -1,9 +0,0 @@
-function addCourse(studentId) {
-	var courseId = $(".selectCourse").val();
-	if (courseId == null || courseId.length == 0) {
-		alert("课程为空，不能添加");
-	} else {
-		window.location = "addCourse?studentId=" + studentId + "&courseId="
-				+ courseId;
-	}
-}
